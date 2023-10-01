ee7ad6660305f860795df9d235416f3f2e162dab
LunaTheFox20 <80362356+LunaTheFox20@users.noreply.github.com>
2023-10-01 14:21:41 +0200

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -4,21 +4,6 @@ import math
 EUCLIDEAN_METHOD = "euclidean"
 MANHATTAN_METHOD = "manhattan"
 
-def euclidean_distance(point1, point2):
-    """
-    Calculate the Euclidean distance between two 3D points.
-
-    Args:
-    point1 (tuple): A tuple containing (x, y, z) coordinates of the first point.
-    point2 (tuple): A tuple containing (x, y, z) coordinates of the second point.
-
-    Returns:
-    float: The Euclidean distance between the two points.
-    """
-    x1, y1, z1 = point1
-    x2, y2, z2 = point2
-    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
-
 def manhattan_distance(point1, point2):
     """
     Calculate the Manhattan distance between two 3D points.
@@ -32,18 +17,25 @@ def manhattan_distance(point1, point2):
     """
     x1, y1, z1 = point1
     x2, y2, z2 = point2
-    return abs(x2 - x1) + abs(y2 - y1) + abs(z2 - z1)
+    dx, dy, dz = abs(x2 - x1), abs(y2 - y1), abs(z2 - z1)
+    return dx + dy + dz
 
-def get_point_input(prompt):
+distance_functions = {
     """
-    Get 3D point coordinates (x, y, z) from the user.
+    Calculate the Euclidean distance between two 3D points.
 
     Args:
-    prompt (str): The input prompt message.
+    point1 (tuple): A tuple containing (x, y, z) coordinates of the first point.
+    point2 (tuple): A tuple containing (x, y, z) coordinates of the second point.
 
     Returns:
-    tuple: A tuple containing (x, y, z) coordinates.
+    float: The Euclidean distance between the two points.
     """
+    EUCLIDEAN_METHOD: lambda point1, point2: round(math.dist(point1, point2)),
+    MANHATTAN_METHOD: manhattan_distance
+}
+
+def get_point_input(prompt):
     input_str = input(prompt)
     x, y, z = map(float, input_str.split())
     return (x, y, z)
@@ -54,17 +46,15 @@ while True:
         point2 = get_point_input("Enter Point 2 (x y z): ")
 
         method = input("Enter the distance method to be used (euclidean/manhattan): ").lower()
-
-        if method == EUCLIDEAN_METHOD:
-            distance = euclidean_distance(point1, point2)
-        elif method == MANHATTAN_METHOD:
-            distance = manhattan_distance(point1, point2)
-        else:
+        
+        try:
+            distance_function = distance_functions[method]
+        except KeyError:
             print("Invalid distance method. Please enter 'euclidean' or 'manhattan'.")
-            continue  # Repeat the loop to ask for a valid method
-
-        print(f"{method.capitalize()} Distance: {distance:.0f}")
-        break  # Exit the loop if everything is successful
+        else:
+            distance = distance_function(point1, point2)
+            print(f"{method.capitalize()} Distance: {distance}")
+            break
 
     except ValueError:
         print("Invalid input. Please enter valid floating-point coordinates.")

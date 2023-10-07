c2570aa0e7be6c1868c78e22750eebc99d238e63
LunaTheFox20 <80362356+LunaTheFox20@users.noreply.github.com>
2023-10-07 20:01:42 +0200

    Update index.ts

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -5,38 +5,41 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-
 // Constants for the Limitations
+const LIMITS = {
+  X: { min: -30000000, max: 30000000 },
+  Y: { min: -319, max: 319 },
+  Z: { min: -30000000, max: 30000000 },
+};
 
-const MAX_X = 30000000;
-const MIN_X = -30000000;
-const MAX_Y = 319; // Maximum build height
-const MIN_Y = -319;
-const MAX_Z = 30000000;
-const MIN_Z = -30000000;
+// Enum for distance methods
+enum DistanceMethod {
+  EUCLIDEAN = 'euclidean',
+  MANHATTAN = 'manhattan',
+}
 
-// The function used to get the
-// Euclidean Distance
-
+// Function to calculate Euclidean Distance
 function calculateEuclideanDistance(coords1: number[], coords2: number[]): number {
-  const dx = coords1[0] - coords2[0];
-  const dy = coords1[1] - coords2[1];
-  const dz = coords1[2] - coords2[2];
+  const [x1, y1, z1] = coords1;
+  const [x2, y2, z2] = coords2;
+  const dx = x1 - x2;
+  const dy = y1 - y2;
+  const dz = z1 - z2;
   return Math.round(Math.sqrt(dx * dx + dy * dy + dz * dz));
 }
 
-// The function used to get the
-// Manhattan Distance
-
+// Function to calculate Manhattan Distance
 function calculateManhattanDistance(coords1: number[], coords2: number[]): number {
-  const dx = Math.abs(coords1[0] - coords2[0]);
-  const dy = Math.abs(coords1[1] - coords2[1]);
-  const dz = Math.abs(coords1[2] - coords2[2]);
+  const [x1, y1, z1] = coords1;
+  const [x2, y2, z2] = coords2;
+  const dx = Math.abs(x1 - x2);
+  const dy = Math.abs(y1 - y2);
+  const dz = Math.abs(z1 - z2);
   return dx + dy + dz;
 }
 
-// Prompt the User to pass the coordinates
-function promptCoordinates(promptText: string, minBounds: number[], maxBounds: number[]): Promise<number[]> {
+// Function to prompt for coordinates
+async function promptCoordinates(promptText: string): Promise<number[]> {
   return new Promise((resolve, reject) => {
     rl.question(promptText, (input) => {
       const coordinates = input
@@ -44,68 +47,40 @@ function promptCoordinates(promptText: string, minBounds: number[], maxBounds: n
         .split(' ')
         .map((coord) => parseFloat(coord.trim()));
 
-      if (!validateCoordinates(coordinates, minBounds, maxBounds)) {
-        reject(new Error(`Invalid input. Please enter three space-separated numbers within the limits: X (${minBounds[0]} - ${maxBounds[0]}), Y (${minBounds[1]} - ${maxBounds[1]}), Z (${minBounds[2]} - ${maxBounds[2]})`));
-      } else {
+      if (
+        coordinates.length === 3 &&
+        coordinates.every((coord) => !isNaN(coord)) &&
+        validateCoordinateRange(coordinates)
+      ) {
         resolve(coordinates);
+      } else {
+        reject(new Error(`Invalid input. Please enter three space-separated numbers within the limits.`));
       }
     });
   });
 }
 
-// Making sure that the coordinate
-// range is not higher or lower
-// than allowed
-
-function validateCoordinateRange(coord: number, min: number, max: number): boolean {
-  return coord >= min && coord <= max;
-}
-
-
-// Making sure the Coordinates are
-// numbers
-function validateCoordinateInput(coord: number): boolean {
-  return !isNaN(coord);
-}
-
-// Custom Function to validate the Coordinates
-// to make sure they're not exceeding the limitations
-function validateCoordinates(coordinates: number[], minBounds: number[], maxBounds: number[]): boolean {
-  return (
-    coordinates.length === 3 &&
-    validateCoordinateInput(coordinates[0]) &&
-    validateCoordinateInput(coordinates[1]) &&
-    validateCoordinateInput(coordinates[2]) &&
-    validateCoordinateRange(coordinates[0], minBounds[0], maxBounds[0]) &&
-    validateCoordinateRange(coordinates[1], minBounds[1], maxBounds[1]) &&
-    validateCoordinateRange(coordinates[2], minBounds[2], maxBounds[2])
-  );
+// Function to validate coordinate range
+function validateCoordinateRange(coords: number[]): boolean {
+  return coords.every((coord, index) => coord >= LIMITS[Object.keys(LIMITS)[index]].min && coord <= LIMITS[Object.keys(LIMITS)[index]].max);
 }
 
 async function main() {
   try {
-    const coords1 = await promptCoordinates(
-      `Enter the first set of coordinates (X ${MIN_X} - ${MAX_X}, Y ${MIN_Y} - ${MAX_Y}, Z ${MIN_Z} - ${MAX_Z}): `,
-      [MIN_X, MIN_Y, MIN_Z],
-      [MAX_X, MAX_Y, MAX_Z]
-    );
-    const coords2 = await promptCoordinates(
-      `Enter the second set of coordinates (X ${MIN_X} - ${MAX_X}, Y ${MIN_Y} - ${MAX_Y}, Z ${MIN_Z} - ${MAX_Z}): `,
-      [MIN_X, MIN_Y, MIN_Z],
-      [MAX_X, MAX_Y, MAX_Z]
-    );
+    const coords1 = await promptCoordinates(`Enter the first set of coordinates: `);
+    const coords2 = await promptCoordinates(`Enter the second set of coordinates: `);
 
-    const distanceMethod = await new Promise<string>((resolve) => {
+    const distanceMethod = await new Promise<DistanceMethod>((resolve) => {
       rl.question('Choose the distance method (Euclidean or Manhattan): ', (method) => {
-        resolve(method.trim().toLowerCase());
+        resolve(method.trim().toLowerCase() as DistanceMethod);
       });
     });
 
     let distance: number;
 
-    if (distanceMethod === 'euclidean') {
+    if (distanceMethod === DistanceMethod.EUCLIDEAN) {
       distance = calculateEuclideanDistance(coords1, coords2);
-    } else if (distanceMethod === 'manhattan') {
+    } else if (distanceMethod === DistanceMethod.MANHATTAN) {
       distance = calculateManhattanDistance(coords1, coords2);
     } else {
       throw new Error('Invalid distance method. Please choose "Euclidean" or "Manhattan".');

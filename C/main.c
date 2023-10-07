fb96ca2cfd71992d1030b63168d8fd5ffa4cce51
LunaTheFox20 <80362356+LunaTheFox20@users.noreply.github.com>
2023-10-07 13:14:31 +0200

    Update main.c
    Forgot to add comments because I'm bored

--- C/main.c
+++ C/main.c
@@ -10,15 +10,18 @@
 #include <math.h>
 #include <ctype.h>
 
+// Define prompt messages for user input
 #define PROMPT_POINT1 "Enter coordinates for Point 1 in the format 'x y z': "
 #define PROMPT_POINT2 "Enter coordinates for Point 2 in the format 'x y z': "
 #define PROMPT_METHOD "Enter the distance calculation method (euclidean or manhattan): "
 
+// Define a struct to represent a 3D point with x, y, and z coordinates
 struct Point
 {
     double x, y, z;
 };
 
+// Function to safely read a line of input from the user
 void safeInput(char *buffer, size_t bufferSize)
 {
     if (fgets(buffer, bufferSize, stdin) == NULL)
@@ -27,11 +30,13 @@ void safeInput(char *buffer, size_t bufferSize)
         exit(EXIT_FAILURE);
     }
 
+    // Remove the newline character if it exists at the end of the input
     size_t len = strlen(buffer);
     if (len > 0 && buffer[len - 1] == '\n')
         buffer[len - 1] = '\0';
 }
 
+// Function to input and validate a 3D point from the user
 void inputAndValidatePoint(struct Point *point, const char *prompt)
 {
     char inputBuffer[100];
@@ -39,6 +44,7 @@ void inputAndValidatePoint(struct Point *point, const char *prompt)
     printf("%s", prompt);
     safeInput(inputBuffer, sizeof(inputBuffer));
 
+    // Continue prompting until three valid numbers are entered
     while (sscanf(inputBuffer, "%lf %lf %lf", &point -> x, &point -> y, &point -> z) != 3)
     {
         fprintf(stderr, "Invalid input. Please enter three valid numbers only.\n");
@@ -47,10 +53,12 @@ void inputAndValidatePoint(struct Point *point, const char *prompt)
     }
 }
 
+// Function to calculate the distance between two points using the specified method
 double calculateDistance(const struct Point *point1, const struct Point *point2, const char *method)
 {
     if (strcmp(method, "euclidean") == 0)
     {
+        // Calculate Euclidean distance
         double dx = point2 -> x - point1 -> x;
         double dy = point2 -> y - point1 -> y;
         double dz = point2 -> z - point1 -> z;
@@ -58,6 +66,7 @@ double calculateDistance(const struct Point *point1, const struct Point *point2,
     }
     else if (strcmp(method, "manhattan") == 0)
     {
+        // Calculate Manhattan distance
         return fabs(point2 -> x - point1 -> x) + fabs(point2 -> y - point1 -> y) + fabs(point2 -> z - point1 -> z);
     }
     else
@@ -69,20 +78,25 @@ double calculateDistance(const struct Point *point1, const struct Point *point2,
 
 int main()
 {
+    // Declare variables to store two 3D points and the distance calculation method
     struct Point point1, point2;
     char method[20];
 
+    // Input and validate the coordinates for Point 1 and Point 2
     inputAndValidatePoint(&point1, PROMPT_POINT1);
     inputAndValidatePoint(&point2, PROMPT_POINT2);
 
+    // Prompt the user for the distance calculation method
     printf("%s", PROMPT_METHOD);
     safeInput(method, sizeof(method));
 
+    // Convert the method to lowercase for case-insensitive comparison
     for (int i = 0; method[i]; i++)
     {
         method[i] = tolower(method[i]);
     }
 
+    // Calculate and display the distance between the two points using the specified method
     double distance = calculateDistance(&point1, &point2, method);
     printf("The %s distance between the two points is: %lf\n", method, distance);
 

883d6e75546b77df9ffebf96faca64338aa43be6
LunaTheFox20 <80362356+LunaTheFox20@users.noreply.github.com>
2023-10-07 20:59:09 +0200

    Update index.ts
    Here's what changed:
    
    1. Better Error Handling:
    
    In the newest version, more comprehensive error handling is added to handle different error scenarios. For example, it checks if the entered distance method is valid, and if not, it provides an error message and asks the user to choose a valid method.
    When input coordinates are invalid (for example, not numeric or out of range), it provides a clear error message and exits the program with an error code (status code 1).
    
    2. Improved User Interaction:
    
    The updated version includes a selectDistanceMethod function that keeps prompting the user until a valid distance method (either "Euclidean" or "Manhattan") is chosen. This ensures that the user selects a valid option.
    It provides more user-friendly error messages that guide the user on what they should do to correct their input.
    
    3. Enhanced Code Readability:
    
    The code is slightly refactored for better readability, such as using a selectDistanceMethod function to handle distance method selection, making the code flow more clear.
    
    4. Consistent Code Style:
    
    The code adheres to consistent indentation and style conventions, making it easier to read and maintain.
    Overall, the updated code aims to provide a better user experience by guiding the user through the input process, handling errors more gracefully, and ensuring that valid input is selected. It also follows best practices for code readability and maintainability.

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,97 +1,114 @@
 import * as readline from 'readline';
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
 // Constants for the Limitations
 const LIMITS = {
-  X: { min: -30000000, max: 30000000 },
-  Y: { min: -319, max: 319 },
-  Z: { min: -30000000, max: 30000000 },
+    X: { min: -30000000, max: 30000000 },
+    Y: { min: -319, max: 319 },
+    Z: { min: -30000000, max: 30000000 },
 };
 
 // Enum for distance methods
 enum DistanceMethod {
-  EUCLIDEAN = 'euclidean',
-  MANHATTAN = 'manhattan',
+    EUCLIDEAN = 'euclidean',
+    MANHATTAN = 'manhattan',
 }
 
 // Function to calculate Euclidean Distance
 function calculateEuclideanDistance(coords1: number[], coords2: number[]): number {
-  const [x1, y1, z1] = coords1;
-  const [x2, y2, z2] = coords2;
-  const dx = x1 - x2;
-  const dy = y1 - y2;
-  const dz = z1 - z2;
-  return Math.round(Math.sqrt(dx * dx + dy * dy + dz * dz));
+    const [x1, y1, z1] = coords1;
+    const [x2, y2, z2] = coords2;
+    const dx = x1 - x2;
+    const dy = y1 - y2;
+    const dz = z1 - z2;
+    return Math.round(Math.sqrt(dx * dx + dy * dy + dz * dz));
 }
 
 // Function to calculate Manhattan Distance
 function calculateManhattanDistance(coords1: number[], coords2: number[]): number {
-  const [x1, y1, z1] = coords1;
-  const [x2, y2, z2] = coords2;
-  const dx = Math.abs(x1 - x2);
-  const dy = Math.abs(y1 - y2);
-  const dz = Math.abs(z1 - z2);
-  return dx + dy + dz;
+    const [x1, y1, z1] = coords1;
+    const [x2, y2, z2] = coords2;
+    const dx = Math.abs(x1 - x2);
+    const dy = Math.abs(y1 - y2);
+    const dz = Math.abs(z1 - z2);
+    return dx + dy + dz;
 }
 
-// Function to prompt for coordinates
-async function promptCoordinates(promptText: string): Promise<number[]> {
-  return new Promise((resolve, reject) => {
-    rl.question(promptText, (input) => {
-      const coordinates = input
-        .trim()
-        .split(' ')
-        .map((coord) => parseFloat(coord.trim()));
-
-      if (
+function validateCoordinates(coordinates: number[]): boolean {
+    return (
         coordinates.length === 3 &&
-        coordinates.every((coord) => !isNaN(coord)) &&
-        validateCoordinateRange(coordinates)
-      ) {
-        resolve(coordinates);
-      } else {
-        reject(new Error(`Invalid input. Please enter three space-separated numbers within the limits.`));
-      }
-    });
-  });
+        coordinates.every((coord) => !isNaN(coord) && validateCoordinateRange(coordinates))
+    );
 }
 
 // Function to validate coordinate range
 function validateCoordinateRange(coords: number[]): boolean {
-  return coords.every((coord, index) => coord >= LIMITS[Object.keys(LIMITS)[index]].min && coord <= LIMITS[Object.keys(LIMITS)[index]].max);
+    return coords.every((coord, index) => {
+        const key = Object.keys(LIMITS)[index] as keyof typeof LIMITS;
+        return coord >= LIMITS[key].min && coord <= LIMITS[key].max;
+    });
+}
+
+// Function to prompt for coordinates
+async function promptCoordinates(promptText: string): Promise<number[]> {
+    return new Promise((resolve, reject) => {
+        rl.question(promptText, (input) => {
+            const coordinates = input
+                .trim()
+                .split(' ')
+                .map((coord) => parseFloat(coord.trim()));
+
+            if (validateCoordinates(coordinates)) {
+                resolve(coordinates);
+            } else {
+                console.error(`Invalid input. Please enter valid numbers for X (${LIMITS.X.min} - ${LIMITS.X.max}), Y (${LIMITS.Y.min} - ${LIMITS.Y.max}), and Z (${LIMITS.Z.min} - ${LIMITS.Z.max}).`);
+                process.exit(1); // Exit the program on invalid input
+            }
+        });
+    });
+}
+
+
+// Function to select a valid distance method
+async function selectDistanceMethod(): Promise<DistanceMethod> {
+    while (true) {
+        const distanceMethod = await new Promise<DistanceMethod>((resolve) => {
+            rl.question('Choose the distance method (Euclidean or Manhattan): ', (method) => {
+                resolve(method.trim().toLowerCase() as DistanceMethod);
+            });
+        });
+
+        if (distanceMethod === DistanceMethod.EUCLIDEAN || distanceMethod === DistanceMethod.MANHATTAN) {
+            return distanceMethod;
+        } else {
+            console.error('Invalid distance method. Please choose "Euclidean" or "Manhattan".');
+        }
+    }
 }
 
 async function main() {
-  try {
-    const coords1 = await promptCoordinates(`Enter the first set of coordinates: `);
-    const coords2 = await promptCoordinates(`Enter the second set of coordinates: `);
+    try {
+        const coords1 = await promptCoordinates(`Enter the first set of coordinates: `);
+        const coords2 = await promptCoordinates(`Enter the second set of coordinates: `);
 
-    const distanceMethod = await new Promise<DistanceMethod>((resolve) => {
-      rl.question('Choose the distance method (Euclidean or Manhattan): ', (method) => {
-        resolve(method.trim().toLowerCase() as DistanceMethod);
-      });
-    });
+        const distanceMethod = await selectDistanceMethod();
 
-    let distance: number;
+        let distance: number;
 
-    if (distanceMethod === DistanceMethod.EUCLIDEAN) {
-      distance = calculateEuclideanDistance(coords1, coords2);
-    } else if (distanceMethod === DistanceMethod.MANHATTAN) {
-      distance = calculateManhattanDistance(coords1, coords2);
-    } else {
-      throw new Error('Invalid distance method. Please choose "Euclidean" or "Manhattan".');
+        if (distanceMethod === DistanceMethod.EUCLIDEAN) {
+            distance = calculateEuclideanDistance(coords1, coords2);
+        } else {
+            distance = calculateManhattanDistance(coords1, coords2);
+        }
+
+        console.log(`Distance: ${distance}`);
+    } finally {
+        rl.close();
     }
-
-    console.log(`Distance: ${distance}`);
-  } catch (error) {
-    console.error(error);
-  } finally {
-    rl.close();
-  }
 }
 
 main();

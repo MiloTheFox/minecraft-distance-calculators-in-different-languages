8e154d552b300fe937d6afce8ac3c0db1ca0765f
LunaTheFox20 <80362356+LunaTheFox20@users.noreply.github.com>
2023-10-12 19:57:48 +0200

    Update main.jl

--- Julia/main.jl
+++ Julia/main.jl
@@ -1,63 +1,47 @@
-# Function to calculate Euclidean Distance
-function euclidean_distance(x1::Float64, y1::Float64, z1::Float64, x2::Float64, y2::Float64, z2::Float64)::Float64
-    return round(sqrt((x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2))
-end
+using Distances
 
-# Function to calculate Manhattan Distance
-function manhattan_distance(x1::Float64, y1::Float64, z1::Float64, x2::Float64, y2::Float64, z2::Float64)::Float64
-    return round(abs(x2 - x1) + abs(y2 - y1) + abs(z2 - z1))
-end
-
-function choose_distance_method()
+function get_distance_method()
     while true
         print("Choose a distance method (Euclidean or Manhattan): ")
         method = lowercase(readline())
 
         if method == "euclidean"
-            return euclidean_distance
+            return Euclidean()
         elseif method == "manhattan"
-            return manhattan_distance
+            return Cityblock()
         else
             println("Invalid input. Please enter 'Euclidean' or 'Manhattan'.")
         end
     end
 end
 
-function get_point_coordinates(point_name::String)::Tuple{Float64, Float64, Float64}
+function get_point_coordinates(point_name::String)::Vector{Float64}
     while true
+        print("Enter coordinates for the ", point_name, " point (x y z): ")
+        input = readline()
+        coords = split(input)
+
+        if length(coords) != 3
+            println("You must enter exactly 3 coordinates for each point.")
+            continue
+        end
+
         try
-            print("Enter coordinates for the ", point_name, " point (x y z): ")
-            input = readline()
-            if !isa(input, AbstractString)
-                throw(ArgumentError("Expected string input"))
-            end
-            x, y, z = input |> split |> x -> parse.(Float64, x)
-
-            if length([x, y, z]) != 3
-                throw(ArgumentError("You must enter exactly 3 coordinates for each point."))
-            end
-
-            return (x, y, z)
+            return parse.(Float64, coords)
         catch e
-            if isa(e, ArgumentError)
-                println("Error: ", e)
-            else
-                rethrow(e)
-            end
+            println("Error: ", e)
         end
     end
 end
 
-
-
 function calculate_and_print_distance()
-    x1, y1, z1 = get_point_coordinates("first")
-    x2, y2, z2 = get_point_coordinates("second")
+    p1 = get_point_coordinates("first")
+    p2 = get_point_coordinates("second")
 
-    distance_method = choose_distance_method()
-    distance = distance_method(x1, y1, z1, x2, y2, z2)
+    distance_method = get_distance_method()
+    distance = evaluate(distance_method, p1, p2)
 
-    println("Distance using ", distance_method == euclidean_distance ? "Euclidean" : "Manhattan", " method: ", distance)
+    println("Distance using ", distance_method == Euclidean() ? "Euclidean" : "Manhattan", " method: ", round(distance))
 end
 
 calculate_and_print_distance()

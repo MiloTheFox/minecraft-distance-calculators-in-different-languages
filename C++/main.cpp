c39798f71fb77d73b9a1bd7de87fc88956888ff1
LunaTheFox20 <80362356+LunaTheFox20@users.noreply.github.com>
2023-10-24 21:03:04 +0200

    Update main.cpp

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,98 +1,88 @@
-/**
- * @author LunaTheFox20
- * @license MIT
- * @version v1.2, 10/10/2023 - 06:26PM GMT+1
- */
-
 #include <iostream>
 #include <cmath>
 #include <string>
-#include <cctype>
 #include <sstream>
 #include <algorithm>
+#include <iomanip> // Include the <iomanip> library
 
-// Define a struct to represent a 3D point with x, y, and z coordinates
 struct Point
 {
     double x, y, z;
 };
 
-// Function to print an error message using std::cerr
 void printErrorMessage(const std::string &message)
 {
     std::cerr << message << std::endl;
 }
 
-// Function to validate and parse a string as three double values into a Point struct
-bool validateDoubleInput(const std::string &input, Point &point)
+bool parseDouble(const std::string &str, double &value)
+{
+    std::istringstream iss(str);
+    return (iss >> value) && iss.eof();
+}
+
+bool validateAndParsePoint(const std::string &input, Point &point)
 {
     std::istringstream iss(input);
-    return (iss >> point.x >> point.y >> point.z) && !iss.fail();
+    return (iss >> point.x >> point.y >> point.z) && iss.eof();
 }
 
-// Function to convert a string to lowercase in place using std::transform
-void toLower(std::string &str)
+std::string toLower(const std::string &str)
 {
-    std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+    std::string result = str;
+    std::transform(result.begin(), result.end(), result.begin(), ::tolower);
+    return result;
 }
 
-// Function to read a Point from user input with validation
 Point readPoint(const std::string &prompt)
 {
     Point point;
     std::string input;
 
-    std::cout << prompt;
     while (true)
     {
-        std::getline(std::cin, input);
-        if (validateDoubleInput(input, point))
-            break;
-        printErrorMessage("Invalid input. Please enter three valid numbers only.");
         std::cout << prompt;
-    }
+        std::getline(std::cin, input);
 
-    return point;
+        if (validateAndParsePoint(input, point))
+            return point;
+
+        printErrorMessage("Invalid input. Please enter three valid numbers only.");
+    }
 }
 
-// Define an enum type for the distance calculation methods
-enum Method
+enum class Method
 {
     EUCLIDEAN,
     MANHATTAN
 };
 
-// Function to read the distance calculation method from user input and return the corresponding enum value
 Method readMethod()
 {
-    std::string method;
-
     while (true)
     {
         std::cout << "Enter the distance calculation method (euclidean or manhattan): ";
+        std::string method;
         std::cin >> method;
-        toLower(method);
 
-        if (method == "euclidean")
-        {
-            return EUCLIDEAN;
-        }
-        else if (method == "manhattan")
-        {
-            return MANHATTAN;
-        }
+        if (toLower(method) == "euclidean")
+            return Method::EUCLIDEAN;
+        if (toLower(method) == "manhattan")
+            return Method::MANHATTAN;
 
         printErrorMessage("Invalid method. Please enter 'euclidean' or 'manhattan'.");
     }
 }
 
-// Function to calculate Euclidean distance between two points using std::hypot
 double calculateEuclideanDistance(const Point &p1, const Point &p2)
 {
-    return std::round(std::hypot(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z));
+    double dx = p1.x - p2.x;
+    double dy = p1.y - p2.y;
+    double dz = p1.z - p2.z;
+    
+    return std::round(std::sqrt(dx * dx + dy * dy + dz * dz));
 }
 
-// Function to calculate Manhattan distance between two points using std::abs
 double calculateManhattanDistance(const Point &p1, const Point &p2)
 {
     return std::abs(p2.x - p1.x) + std::abs(p2.y - p1.y) + std::abs(p2.z - p1.z);
@@ -100,31 +90,29 @@ double calculateManhattanDistance(const Point &p1, const Point &p2)
 
 int main()
 {
-    // Read and validate coordinates for Point 1 and Point 2
     Point point1 = readPoint("Enter coordinates for Point 1 in the format 'x y z': ");
     Point point2 = readPoint("Enter coordinates for Point 2 in the format 'x y z': ");
     
-    // Read the distance calculation method
     Method method = readMethod();
+    
+    double distance = 0;
 
-    double distance;
-
-    // Calculate the distance based on the chosen method using switch statement
     switch (method)
     {
-    case EUCLIDEAN:
-        distance = calculateEuclideanDistance(point1, point2);
-        break;
-    case MANHATTAN:
-        distance = calculateManhattanDistance(point1, point2);
-        break;
-    default:
-        printErrorMessage("Invalid method.");
-        return 1;
+        case Method::EUCLIDEAN:
+            distance = calculateEuclideanDistance(point1, point2);
+            break;
+        case Method::MANHATTAN:
+            distance = calculateManhattanDistance(point1, point2);
+            break;
+        default:
+            printErrorMessage("Invalid method.");
+            return 1;
     }
 
-    // Display the calculated distance
-    std::cout << "The " << (method == EUCLIDEAN ? "euclidean" : "manhattan") << " distance between the two points is: " << distance << std::endl;
+    // Use std::fixed and std::setprecision to format the output with 2 digits after the decimal point
+    std::cout << "The " << (method == Method::EUCLIDEAN ? "euclidean" : "manhattan") << " distance between the two points is: ";
+    std::cout << std::fixed << std::setprecision(2) << distance << std::endl;
 
     return 0;
 }

67628eb1475db013463f0424020553cef30f4866
LunaTheFox20 <80362356+LunaTheFox20@users.noreply.github.com>
2023-10-26 20:25:16 +0200

    Update main.rs

--- Rust/main.rs
+++ Rust/main.rs
@@ -1,70 +1,82 @@
-use std::io::{self};
+use std::io;
 
-const PROMPT_POINT_1: &str = "Enter the first point (x1 y1 z1): ";
-const PROMPT_POINT_2: &str = "Enter the second point (x2 y2 z2): ";
-const PROMPT_CHOICE: &str = "Enter your choice: ";
+struct Point {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+const PROMPT_CHOICE: &str = "Enter your choice (1 for Euclidean, 2 for Manhattan): ";
 
 fn main() {
-    // Input coordinates
-    let (x1, y1, z1) = get_coordinates(PROMPT_POINT_1).unwrap();
-    let (x2, y2, z2) = get_coordinates(PROMPT_POINT_2).unwrap();
-    // Prompt the user for the distance calculation method
-    println!("1. Euclidean Distance");
-    println!("2. Manhattan Distance");
-    let choice = get_distance_calculation_choice().unwrap();
-    // Calculate and display the selected distance
-    match choice {
+    let point1 = get_coordinates("Enter the first point (x1 y1 z1): ").unwrap();
+    let point2 = get_coordinates("Enter the second point (x2 y2 z2): ").unwrap();
+
+    match get_distance_calculation_choice().unwrap() {
         1 => {
-            let euclidean_distance = euclidean_distance(x1, y1, z1, x2, y2, z2);
-            println!("Euclidean Distance: {:.2}", euclidean_distance)
+            let euclidean_distance = euclidean_distance(&point1, &point2);
+            println!("Euclidean Distance: {:.2}", euclidean_distance);
         }
         2 => {
-            let manhattan_distance = manhattan_distance(x1, y1, z1, x2, y2, z2);
-            println!("Manhattan Distance: {:.2}", manhattan_distance)
+            let manhattan_distance = manhattan_distance(&point1, &point2);
+            println!("Manhattan Distance: {:.2}", manhattan_distance);
         }
         _ => println!("Invalid choice. Please select 1 or 2."),
     }
 }
 
-fn get_coordinates(prompt: &str) -> io::Result<(f64, f64, f64)> {
+fn get_coordinates(prompt: &str) -> io::Result<Point> {
     loop {
         print!("{}", prompt);
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
+
         let coordinates: Result<Vec<f64>, _> = input
             .split_whitespace()
             .map(|s| s.trim().parse())
             .collect();
+
         match coordinates {
-            Ok(coords) if coords.len() == 3 => return Ok((coords[0], coords[1], coords[2])),
+            Ok(coords) if coords.len() == 3 => {
+                return Ok(Point {
+                    x: coords[0],
+                    y: coords[1],
+                    z: coords[2],
+                });
+            }
             _ => println!("Invalid input. Please enter three numbers separated by spaces."),
         }
     }
 }
+
 
 fn get_distance_calculation_choice() -> io::Result<u32> {
     loop {
         print!("{}", PROMPT_CHOICE);
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
-        match input.trim().parse() {
-            Ok(1) | Ok(2) => return Ok(input.trim().parse().unwrap()),
-            _ => println!("Invalid choice. Please select 1 or 2."),
+
+        if let Ok(choice) = input.trim().parse() {
+            if choice == 1 || choice == 2 {
+                return Ok(choice);
+            }
         }
+
+        println!("Invalid choice. Please select 1 or 2.");
     }
 }
 
-fn euclidean_distance(x1: f64, y1: f64, z1: f64, x2: f64, y2: f64, z2: f64) -> f64 {
-    [(x1, x2), (y1, y2), (z1, z2)]
-    .iter()
-    .map(|(a, b)| (b - a).powi(2))
-    .sum::<f64>()
-    .sqrt()
+fn euclidean_distance(point1: &Point, point2: &Point) -> f64 {
+    [(point1.x, point2.x), (point1.y, point2.y), (point1.z, point2.z)]
+        .iter()
+        .map(|(a, b)| (b - a).powi(2))
+        .sum::<f64>()
+        .sqrt()
 }
 
-fn manhattan_distance(x1: f64, y1: f64, z1: f64, x2: f64, y2: f64, z2: f64) -> f64 {
-    [(x1, x2), (y1, y2), (z1, z2)]
-    .iter()
-    .map(|(a, b)| (b - a).abs())
-    .sum::<f64>()
+fn manhattan_distance(point1: &Point, point2: &Point) -> f64 {
+    [(point1.x, point2.x), (point1.y, point2.y), (point1.z, point2.z)]
+        .iter()
+        .map(|(a, b)| (b - a).abs())
+        .sum::<f64>()
 }

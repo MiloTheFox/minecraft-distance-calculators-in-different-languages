0c669480dc094dc24dc429bc777a054c84510dea
LunaTheFox20 <80362356+LunaTheFox20@users.noreply.github.com>
2023-10-28 09:18:08 +0200

    Update main.go

--- Go/main.go
+++ Go/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math"
-	"log"
+	"os"
+	"strings"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 type Point struct {
@@ -16,12 +21,17 @@ const (
 	Manhattan = "manhattan"
 )
 
-func main() {
-	point1 := getPoint("Enter the coordinates of Point 1 (X Y Z):")
-	point2 := getPoint("Enter the coordinates of Point 2 (X Y Z):")
+func titleCase(s string) string {
+	return cases.Title(language.English).String(s)
+}
 
-	fmt.Print("Enter the distance calculation method (euclidean or manhattan): ")
-	method := getUserInput()
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	point1 := getPoint(reader, "Enter the coordinates of Point 1 (X Y Z):")
+	point2 := getPoint(reader, "Enter the coordinates of Point 2 (X Y Z):")
+
+	method := getCalculationMethod(reader)
 
 	var distance float64
 	switch method {
@@ -30,38 +40,51 @@ func main() {
 	case Manhattan:
 		distance = manhattanDistance(point1, point2)
 	default:
-		log.Fatal("Invalid method. Please enter 'euclidean' or 'manhattan'.")
+		fmt.Println("Invalid method. Please enter 'euclidean' or 'manhattan'.")
+		return
 	}
-
-	fmt.Printf("Distance between Point 1 and Point 2: %f\n", distance)
+	fmt.Printf("%s Distance between Point 1 and Point 2: %f\n", titleCase(method), distance)
 }
 
-func getPoint(prompt string) Point {
+func getPoint(reader *bufio.Reader, prompt string) Point {
 	var x, y, z float64
-	fmt.Print(prompt + " ")
-	_, err := fmt.Scanf("%f %f %f\n", &x, &y, &z)
-	if err != nil {
-		log.Fatal("Invalid input:", err)
+	for {
+		fmt.Print(prompt + " ")
+		line, _ := reader.ReadString('\n')
+		n, err := fmt.Sscanf(line, "%f %f %f", &x, &y, &z)
+		if err == nil && n == 3 {
+			return Point{x, y, z}
+		}
+		fmt.Println("Invalid input. Please enter valid coordinates.")
 	}
-	return Point{x, y, z}
 }
 
-func getUserInput() string {
-	var input string
-	fmt.Scanf("%s\n", &input)
-	return input
+func getCalculationMethod(reader *bufio.Reader) string {
+	for {
+		fmt.Print("Enter the distance calculation method (euclidean or manhattan): ")
+		method := strings.ToLower(getUserInput(reader))
+		if method == Euclidean || method == Manhattan {
+			return method
+		}
+		fmt.Println("Invalid method. Please enter 'euclidean' or 'manhattan'.")
+	}
+}
+
+func getUserInput(reader *bufio.Reader) string {
+	input, _ := reader.ReadString('\n')
+	return strings.TrimSpace(input)
 }
 
 func euclideanDistance(p1, p2 Point) float64 {
 	dx := p2.X - p1.X
 	dy := p2.Y - p1.Y
 	dz := p2.Z - p1.Z
-	return math.RoundToEven(math.Sqrt(dx*dx + dy*dy + dz*dz))
+	return math.Sqrt(dx*dx + dy*dy + dz*dz)
 }
 
 func manhattanDistance(p1, p2 Point) float64 {
 	dx := math.Abs(p2.X - p1.X)
 	dy := math.Abs(p2.Y - p1.Y)
 	dz := math.Abs(p2.Z - p1.Z)
-	return math.RoundToEven(dx + dy + dz)
+	return dx + dy + dz
 }

22dcaa7c82d17978ec91c6f40268cf7c7b547a72
LunaTheFox20 <80362356+LunaTheFox20@users.noreply.github.com>
2023-12-15 18:54:11 +0100

    Update main.rs

--- Rust/main.rs
+++ Rust/main.rs
@@ -1,5 +1,7 @@
 use std::io::{self, Write};
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 struct Point {
     x: f64,
     y: f64,
@@ -14,39 +16,46 @@ enum DistanceMethod {
 const PROMPT_CHOICE: &str = "Enter your choice (1 for Euclidean, 2 for Manhattan): ";
 
 fn main() {
-    let point1 = get_point("Enter the first point (x1 y1 z1): ");
-    let point2 = get_point("Enter the second point (x2 y2 z2): ");
-
-    match get_distance_calculation_choice() {
-        Ok(DistanceMethod::Euclidean) => {
-            let euclidean_distance = euclidean_distance(&point1, &point2);
-            println!("Euclidean Distance: {:.2}", euclidean_distance);
-        }
-        Ok(DistanceMethod::Manhattan) => {
-            let manhattan_distance = manhattan_distance(&point1, &point2);
-            println!("Manhattan Distance: {:.2}", manhattan_distance);
-        }
-        _ => println!("Invalid choice. Please select 1 or 2."),
+    match (
+        get_point("Enter the first point (x1 y1 z1): "),
+        get_point("Enter the second point (x2 y2 z2): "),
+    ) {
+        (Ok(point1), Ok(point2)) => match get_distance_calculation_choice() {
+            Ok(DistanceMethod::Euclidean) => {
+                let euclidean_distance = euclidean_distance(&point1, &point2);
+                println!("Euclidean Distance: {:.2}", euclidean_distance);
+            }
+            Ok(DistanceMethod::Manhattan) => {
+                let manhattan_distance = manhattan_distance(&point1, &point2);
+                println!("Manhattan Distance: {:.2}", manhattan_distance);
+            }
+            _ => println!("Invalid choice. Please select 1 or 2."),
+        },
+        _ => println!("Failed to get points. Please enter valid numbers."),
     }
 }
 
-fn get_point(prompt: &str) -> Point {
+fn get_point(prompt: &str) -> Result<Point, io::Error> {
     loop {
         print!("{}", prompt);
-        io::stdout().flush().expect("Failed to flush stdout");
+        io::stdout()
+            .flush()
+            .unwrap_or_else(|_| panic!("Failed to flush stdout"));
 
         let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read input");
+        io::stdin().read_line(&mut input)?;
 
-        let coordinates: Vec<_> = input.trim().split_whitespace().map(|s| s.parse()).collect();
-
-        match coordinates.as_slice() {
-            [Ok(x), Ok(y), Ok(z)] => {
-                return Point { x: *x, y: *y, z: *z }
+        let mut iter = input.trim().split_whitespace().map(|s| s.parse());
+        match (iter.next(), iter.next(), iter.next(), iter.next()) {
+            (Some(Ok(x)), Some(Ok(y)), Some(Ok(z)), None) => {
+                return Ok(Point { x, y, z });
             }
-            _ => println!("Invalid input. Please enter three numbers separated by spaces."),
+            (None, None, None, None) => {
+                println!("No input. Please enter three valid numbers separated by spaces.");
+            }
+            _ => {
+                println!("Invalid input. Please enter three valid numbers separated by spaces.");
+            }
         }
     }
 }
@@ -54,32 +63,39 @@ fn get_point(prompt: &str) -> Point {
 fn get_distance_calculation_choice() -> io::Result<DistanceMethod> {
     loop {
         print!("{}", PROMPT_CHOICE);
-        io::stdout().flush().unwrap();
+        io::stdout()
+            .flush()
+            .unwrap_or_else(|_| panic!("Failed to flush stdout"));
+
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
 
-        match input.trim().parse::<u32>() {
-            Ok(1) => return Ok(DistanceMethod::Euclidean),
-            Ok(2) => return Ok(DistanceMethod::Manhattan),
-            _ => println!("Invalid choice. Please select 1 or 2."),
-        }
+        return Ok(match input.trim().parse::<u32>().unwrap_or(0) {
+            1 => DistanceMethod::Euclidean,
+            2 => DistanceMethod::Manhattan,
+            _ => {
+                println!("Invalid choice. Please select 1 or 2.");
+                continue;
+            }
+        });
     }
 }
 
-fn calculate_differences(point1: &Point, point2: &Point) -> Point {
-    Point {
-        x: point2.x - point1.x,
-        y: point2.y - point1.y,
-        z: point2.z - point1.z,
-    }
+fn distance<F>(point1: &Point, point2: &Point, operation: F) -> f64
+where
+    F: Fn(f64, f64) -> f64,
+{
+    [point1.x, point1.y, point1.z]
+        .iter()
+        .zip([point2.x, point2.y, point2.z].iter())
+        .map(|(&x1, &x2)| operation(x2, x1))
+        .sum()
 }
 
 fn euclidean_distance(point1: &Point, point2: &Point) -> f64 {
-    let diff = calculate_differences(point1, point2);
-    (diff.x.powi(2) + diff.y.powi(2) + diff.z.powi(2)).sqrt()
+    distance(point1, point2, |x2, x1| (x2 - x1).powi(2)).sqrt()
 }
 
 fn manhattan_distance(point1: &Point, point2: &Point) -> f64 {
-    let diff = calculate_differences(point1, point2);
-    diff.x.abs() + diff.y.abs() + diff.z.abs()
+    distance(point1, point2, |x2, x1| (x2 - x1).abs())
 }

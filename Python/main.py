539c0d0d39ccee761739773e9fc69f087e572566
LunaTheFox20 <80362356+LunaTheFox20@users.noreply.github.com>
2024-02-17 22:24:36 +0100

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -6,48 +6,39 @@ MANHATTAN_METHOD = "manhattan"
 
 
 def manhattan_distance(point1, point2):
-    x1, y1, z1 = point1
-    x2, y2, z2 = point2
-    dx, dy, dz = abs(x2 - x1), abs(y2 - y1), abs(z2 - z1)
-    return dx + dy + dz
+    return sum(abs(a - b) for a, b in zip(point1, point2))
 
 
 def get_point_input(prompt):
     while True:
         try:
-            input_str = input(prompt)
-            x, y, z = map(float, input_str.split())
-            return (x, y, z)
+            return tuple(map(float, input(prompt).split()))
         except ValueError:
             print("Invalid input. Please enter valid floating-point coordinates.")
 
 
 def distance_function(point1, point2, method):
     if method == EUCLIDEAN_METHOD:
-        return math.dist(point1, point2)
+        return round(math.dist(point1, point2), 2)
     elif method == MANHATTAN_METHOD:
-        return round(manhattan_distance(point1, point2))
-    else:
-        raise ValueError(
-            "Invalid distance method. Please enter 'euclidean' or 'manhattan'."
-        )
+        return round(manhattan_distance(point1, point2), 2)
+
+
+def get_method_input(prompt):
+    while True:
+        method = input(prompt).lower()
+        if method in [EUCLIDEAN_METHOD, MANHATTAN_METHOD]:
+            return method
+        print("Invalid method. Please enter 'euclidean' or 'manhattan'.")
 
 
 while True:
     try:
         point1 = get_point_input("Enter Point 1 (x y z): ")
         point2 = get_point_input("Enter Point 2 (x y z): ")
-
-        method = input(
-            "Enter the distance method to be used (euclidean/manhattan): "
-        ).lower()
-        if method not in [EUCLIDEAN_METHOD, MANHATTAN_METHOD]:
-            print("Invalid method. Please enter 'euclidean' or 'manhattan'.")
-            continue
-
+        method = get_method_input("Enter the distance method to be used (euclidean/manhattan): ")
         distance = distance_function(point1, point2, method)
         print(f"{method.capitalize()} Distance: {distance}")
         break
-
     except ValueError as e:
         print(e)
